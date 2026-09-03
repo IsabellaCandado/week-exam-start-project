8d22750db3483b7517d5f3c56fc73ab4a19b9a92
IsabellaCandado <isabellacandadoconde@gmail.com>
2026-09-03 15:50:51 -0500

    ok

--- src/main/java/edu/co/icesi/repositories/ExpeditionRepository.java
+++ src/main/java/edu/co/icesi/repositories/ExpeditionRepository.java
@@ -20,7 +20,26 @@ public class ExpeditionRepository {
         Expedition expedition1 = new Expedition();
         expedition1.setId(1);
         expedition1.setName("Expedition 1");
-        expedition1.
+        expedition1.setCode("12345");
+        expedition1.setRegion("Amazonia");
+        expedition1.setBaseCamp("Leticia");
+        expedition1.setLeader("Isabella");
+        expedition1.setStartDate("2026-09-01");
+        expedition1.setEndDate("2026-09-03");
+        expedition1.setState("Completed");
+        expeditions.put(1, expedition1);
+
+        Expedition expedition2 = new Expedition();
+        expedition2.setId(2);
+        expedition2.setName("Expedition 2");
+        expedition2.setCode("54321");
+        expedition2.setRegion("Pacific Coast");
+        expedition2.setBaseCamp("Choco");
+        expedition2.setLeader("Jhoan");
+        expedition2.setStartDate("2026-08-29");
+        expedition2.setEndDate("2026-09-03");
+        expedition2.setState("Completed");
+        expeditions.put(2, expedition2);
     }
 
     public void add(Expedition expedition) {
--- src/main/java/edu/co/icesi/repositories/SightingRepository.java
+++ src/main/java/edu/co/icesi/repositories/SightingRepository.java
@@ -17,7 +17,31 @@ public class SightingRepository {
 
     @PostConstruct
     public void init() {
+        Sighting s1 = new Sighting();
+        s1.setId(1);
+        s1.setSightingCode("123");
+        s1.setName("Sighting 1");
+        s1.setDescription("Bird");
+        s1.setScientificName("Bird");
+        s1.setSightedAt("2026-09-02");
+        s1.setLocation("Leticia");
+        s1.setQuantity(1);
+        s1.setConfidenceLevel(5);
+        s1.setExpeditionId(1);
+        sightings.put(s1.getId(), s1);
 
+        Sighting s2 = new Sighting();
+        s2.setId(2);
+        s2.setSightingCode("321");
+        s2.setName("Sighting 2");
+        s2.setDescription("Turtle");
+        s2.setScientificName("Turtle");
+        s2.setSightedAt("2026-09-02");
+        s2.setLocation("Choco");
+        s2.setQuantity(1);
+        s2.setConfidenceLevel(5);
+        s2.setExpeditionId(2);
+        sightings.put(s2.getId(), s2);
     }
 
     public void add(Sighting sighting) {
